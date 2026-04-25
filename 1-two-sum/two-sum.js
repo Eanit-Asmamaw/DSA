a146772e6edb2969fbdfe6d69e9b8c49504d837c
Eanit-Asmamaw <eanitasmamaw@gmail.com>
2026-04-26 00:53:21 +0300

    Time: 1 ms (85.81%) | Memory: 55.5 MB (35.52%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+ let map = new Map();
+ for( let i=0; i < nums.length; i++)  {
+    let num2 = target - nums[i];
+    if(map.has(num2)){
+        return[i, map.get(num2)];
+    }
+    map.set(nums[i], i);
+ } 
+};
